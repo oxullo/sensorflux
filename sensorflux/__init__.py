0bd1e91fad236d251be7cc40b43aac7f0528cb53
Max Schäth <max.schaeth@gmx.de>
2020-11-19 17:08:43 +0100

    Add python package boilerplate

---
+++ sensorflux/__init__.py
@@ -0,0 +1,5 @@
+"""sensorflux - Service relay for sensors to influxdb"""
+
+__version__ = '0.1.0'
+__author__ = 'Python Team <foo@bar.com>'
+__all__ = []
---
+++ sensorflux/cli.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def run():
+    print('foobar')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(run())
