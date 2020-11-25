6b223d21bef0db045e9c2d2b6843697945bd9fc0
Max Schaeth <max.schaeth@gmx.de>
2020-11-25 22:21:18 +0100

    You will love this I forgot to add the files to the commit but I am practicing

--- sensorflux/arduino_com.py
+++ sensorflux/arduino_com.py
@@ -4,38 +4,53 @@
 import arduino_value_collect
 
 
-class ArduinoCom:
+def get_values_day(date):
+    value_collection = arduino_value_collect.input_collection
+    values_of_day = []
+    print(f'This is value collection {value_collection}')
+#    values_of_day = [i for i in value_collection if i[0] == date]
+    for i in value_collection:
+        print(f'This is i {i}')
+        if i[0] == date:
+            values_of_day.append(i)
+
+    print(f'Values of the Day {values_of_day}')
+    return
 
 
-    def get_last_value(self):
-        value_collection = arduino_value_collect.input_collection
-        latest_values = []
-        for i in value_collection:
-            latest_values.append(i[-1])
+def get_values_between():
+    return
 
-        print(f' This are the latest Values {latest_values}')
-        return latest_values
 
-    def get_latest_values(self, num_samples):
-        value_collection = arduino_value_collect.input_collection
-        latest_values_average = []
-        it = iter(value_collection)
-        next(it, None)
-        for i in it:
-            average = sum(i[-num_samples:]) / num_samples
-            latest_values_average.append(average)
+def get_highest_value():
+    return
 
-        latest_values = []
-        for i in value_collection:
-            for j in i[-num_samples:]:
-                latest_values.append(j)
 
-        print(f' Values average is {latest_values_average}')
-        print(f'Latest values List{latest_values}')
-        return latest_values_average , latest_values
+def get_last_value():
+    value_collection = arduino_value_collect.input_collection
+    last_value = []
+    for i in value_collection:
+        last_value.append(i[-1])
+#    print(f' This are the latest Values {last_value}')
+    return last_value
 
-    def get_values_day(self):
-        return
 
-    def get_values_between(self):
-        return
+def get_average(num_samples):
+    value_collection = arduino_value_collect.input_collection
+    latest_values_average = []
+    it = iter(value_collection)
+    next(it, None)
+    for i in it:
+        average = sum(i[-num_samples:]) / num_samples
+        latest_values_average.append(average)
+#    print(f' Values average is {latest_values_average}')
+    return latest_values_average
+
+
+def get_latest_values(num_sampless):
+    value_collection = arduino_value_collect.input_collection
+    latest_values = []
+    for i in value_collection:
+        latest_values.append(i[-num_sampless:])
+#    print(f'Latest values List{latest_values}')
+    return latest_values
--- sensorflux/arduino_value_collect.py
+++ sensorflux/arduino_value_collect.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# import asyncio
 import random
 from _datetime import datetime
 import threading
@@ -14,47 +13,20 @@ def input_genertator():
     temp = random.randint(1, 255)
     atmo = random.randint(1, 255)
     humi = random.randint(1, 255)
-    dateTime = datetime.now()
-    date_time_cleaned = [dateTime.year, dateTime.month, dateTime.day, dateTime.hour, dateTime.minute, dateTime.second]
+    time = datetime.now()
+    date_time_cleaned = [time.year, time.month, time.day]
     return [date_time_cleaned, temp, atmo, humi]
 
 
 def call_generator():
     threading.Timer(5.0, call_generator).start()
     global input_collection
-    input = input_genertator()
+    values = input_genertator()
     count = 0
     for i in input_collection:
-        i.append(input[count])
+        i.append(values[count])
         count = count + 1
+    return
 
-
-#    print(input_collection)
 
 call_generator()
-# TIME_PAUSE_VALUES = 5
-#
-# ticker = threading.Event()
-# while not ticker.wait(TIME_PAUSE_VALUES):
-#     call_generator()
-
-# async def call_generator():
-#     global input_collection
-#     while True:
-#         input = input_genertator()
-#        # print(input)
-#         #input_collection[0].append(input[0])
-#         #print(input_collection)
-#         count=0
-#         for i in input_collection:
-#             i.append(input[count])
-#             count=count+1
-#         print(f'This is input_collection{input_collection}')
-#
-#         await asyncio.sleep(10)
-#
-#
-# input_loop = asyncio.get_event_loop()
-# input_tasks = input_loop.create_task(call_generator())
-# input_loop.run_forever()
-# input_loop.close()
