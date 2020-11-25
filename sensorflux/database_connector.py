de7e4b604aeeee9b9913b2839eaa7cbe90cda84b
19201080 <etienne.j.n@gmail.com>
2020-11-25 19:19:56 +0100

    Add docstrings to database_connector

--- sensorflux/database_connector.py
+++ sensorflux/database_connector.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+sensorflux.database_connector
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module contains the class that communicates data to influxdb
+"""
+
 from copy import deepcopy
 from datetime import datetime
 from random import random
@@ -9,6 +16,12 @@ from influxdb import InfluxDBClient
 
 
 class DatabaseConnector:
+    """
+    A class to represent a client connection to the influxdb instance
+
+    :param str measurement: measurement name
+    :param str device: name for the device tag
+    """
     def __init__(self, measurement='test_measurement', device='test_device'):
         self.client = InfluxDBClient(
             host='localhost',
@@ -21,11 +34,23 @@ class DatabaseConnector:
 
     @staticmethod
     def check_data(data):
+        """
+        Checks that the data is in a valid format. Returns true if yes
+
+        :param dict data: measurement data
+        :rtype: bool
+        """
         right_type = isinstance(data, dict)
         has_data = any(key in data for key in ('temp', 'atmo', 'humi'))
         return right_type and has_data
 
     def clean_point(self, data):
+        """
+        Formats the data in the right format for influxdb
+
+        :param dict data: measurement data
+        :rtype: dict
+        """
         data_copy = deepcopy(data)
         time = data_copy.pop('time') if 'time' in data_copy \
             else datetime.utcnow().isoformat()
@@ -39,6 +64,14 @@ class DatabaseConnector:
         }
 
     def write(self, data):
+        r"""
+        Sends data to influxdb. Returns true if successful
+
+        :param dict data: measurement data as a dict optionally
+            containing a timestamp in ISO 8601 format, and containing
+            any of 'temp', 'atmo' or 'humi' values
+        :rtype: bool
+        """
         assert self.check_data(data), 'wrong data format'
         point = self.clean_point(data)
         successful = self.client.write_points([point])
