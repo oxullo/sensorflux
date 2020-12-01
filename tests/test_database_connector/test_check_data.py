60dd7ed88cfb479a399f4e04b5d271ccb50bd4c7
19201080 <etienne.j.n@gmail.com>
2020-12-01 19:55:53 +0100

    Add pytest fixture to test_check_data

--- tests/test_database_connector/test_check_data.py
+++ tests/test_database_connector/test_check_data.py
@@ -3,20 +3,26 @@
 
 from datetime import datetime
 
+import pytest
+
 from sensorflux.database_connector import DatabaseConnector
 
 
-def test_check_data():
+@pytest.fixture()
+def connector():
+    connector_instance = DatabaseConnector(
+        measurement='measurement',
+        device='device',
+        fields=('temp', 'atmo', 'humi'))
+    return connector_instance
+
+
+def test_check_data(connector):
     """
     GIVEN data to be sent to influxdb
     WHEN the method is called with data as an argument
     THEN it should only return True if the data is valid
     """
-    connector = DatabaseConnector(
-        measurement='measurement',
-        device='device',
-        fields=('temp', 'atmo', 'humi'))
-
     assert connector.check_data({
         'time': datetime.utcnow().isoformat(),
         'temp': 123,
