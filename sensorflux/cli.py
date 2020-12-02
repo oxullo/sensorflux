297270391eb1e5b044b597e0d10b910ff7c3463a
19201080 <etienne.j.n@gmail.com>
2020-12-02 19:56:14 +0100

    Add basic code to run PollingInterface with fake data sent to influxdb

--- sensorflux/cli.py
+++ sensorflux/cli.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from random import random
 import sys
+
+from sensorflux.database_connector import DatabaseConnector
+from sensorflux.polling_interface import PollingInterface
+
+
+class FakeData:
+    def __init__(self, fields):
+        self._fields = fields
+
+    def read(self):
+        return {field: random() * 100 for field in self._fields}
 
 
 def run():
-    print('foobar')
-    return 0
+    fields = ('temp', 'atmo', 'humi')
+    fake_client = FakeData(fields)
+    db_client = DatabaseConnector('test_measurement', 'test_device', fields)
+    poller = PollingInterface(fake_client, db_client, 5)
+    try:
+        poller.run()
+    except KeyboardInterrupt:
+        print('shutting down')
+        return 0
+    except AttributeError as e:
+        print(e)
+    return 1
 
 
 if __name__ == '__main__':
