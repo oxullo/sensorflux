159f40046d347305cd0a87d91e584a946f78df0e
19201080 <etienne.j.n@gmail.com>
2020-12-03 17:29:26 +0100

    Add basic logging configuration with debug mode and formatting

--- sensorflux/cli.py
+++ sensorflux/cli.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
 import sys
 
+import click
 
-def run():
+logger = logging.getLogger(__name__)
+LOG_FORMAT = ('%(asctime)-15s [%(levelname)-7s]: '
+              '%(message)s (%(filename)s:%(lineno)s)')
+
+
+@click.command()
+@click.option('--debug/--no-debug', default=False)
+def run(debug):
+    logging.basicConfig(
+        format=LOG_FORMAT,
+        level=logging.DEBUG if debug else logging.INFO)
+    logger.debug('debug')
+    logger.info('info')
     print('foobar')
     return 0
 
